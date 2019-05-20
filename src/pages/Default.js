ae0cb6fdabe98b5ee7dae2166515efdc64fe0eb6
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-20 13:17:43 +0100

    styling error page

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import {Link} from 'react-router-dom';
 
 export default function Default() {
   return (
-    <h4>
-      Error Page
-    </h4>
+    <Header title="404" styleClass='default-hero'>
+      <h2 className="text-light text-uppercase">url does not exist! :( </h2>
+      <Link to="/" className="text-uppercase btn btn-secondary btn-large mt-3">
+        return home
+      </Link>
+    </Header>
   )
 }
+ 
